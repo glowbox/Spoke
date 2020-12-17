7a1187d8674a17b4013b266503dfb0e49974471b
Thomas Wester <thomas@thomaswester.com>
2020-12-16 22:21:33 -0800

    vpt-stream property build out (pointsize, scale)

--- src/editor/nodes/VPTStreamNode.js
+++ src/editor/nodes/VPTStreamNode.js
@@ -9,8 +9,9 @@ import { buildAbsoluteURL } from "url-toolkit";
 import { proxiedUrlFor } from "../../api/Api";
 import configs from '../../configs'
 import loadTexture from "../utils/loadTexture";
-
+import { Vector2 } from "three";
 import "depthkit";
+import Hls from "hls.js/dist/hls.light";
 
 //let linkHelperTexture = null;
 
@@ -41,14 +42,24 @@ export default class VPTStreamNode extends EditorNodeMixin(Object3D) {
   constructor(editor) {
     super(editor);
 
+    window.Hls = Hls;
+
     //TODO: refactor to match how audiosource/videosource, where the base functionality is an "object" and the node is a simple wrapper around it
-    this._src = "";
-    this._meta = realsense415;
+
+    this.params = {
+      src: "",
+      meta: realsense415,
+      sensor: "realsenseD415",
+      renderMode: "perspective",
+      pointSize: 8.0,
+      thresholdX: { min: -2.0, max: 2.0 },
+      thresholdY: { min: -2.0, max: 2.0 },
+      thresholdZ: { min: 0.0, max: 4.0 },
+    }
 
     this.vptstream = new VPTStream();
     this.vptstream.nodeName = "IgnoreForExport";
     this.vptstream.hls_xhroverride = this.proxyHLS;
-    document.body.appendChild(this.vptstream.video);
 
     this.vptstream.addEventListener(STREAMEVENTS.PLAY_SUCCESS, function (event) {
       console.log(`${event.type} ${event.message}`);
@@ -76,36 +87,94 @@ export default class VPTStreamNode extends EditorNodeMixin(Object3D) {
   }
 
   get src() {
-    return this._src;
+    return this.params.src;
   }
 
   set src(value) {
     if (value) {
       console.log("vptstream set src " + value);
-      this._src = value;
+      this.params.src = value;
       this.loadMedia().catch(console.error);
     }
   }
 
   //TODO: consider embedding the meta data json instead of a url to the data
   get meta() {
-    return this._meta;
+    return this.params.meta;
   }
 
   set meta(value) {
     if (value) {
       console.log("vptstream set meta " + value);
-      this._meta = value;
-      this.loadMedia().catch(console.error);
+      this.params.meta = value;
+      //this.loadMedia().catch(console.error);
     }
   }
 
+  get sensor() { return this.params.sensor; }
+
+  set sensor(value) {
+    //TODO: add support for more sensor calibrations
+    if (value == "realsenseD415") {
+      this.params.meta = realsense415;
+    }
+    this.params.sensor = value;
+  }
+
+  get renderMode() { return this.params.renderMode; }
+
+  set renderMode(value) {
+    if (!value) return;
+    this.params.renderMode = value;
+
+    this.loadMedia().catch(console.error);
+  }
+
+  get pointSize() { return this.params.pointSize; }
+
+  set pointSize(value) {
+    if (!value) return;
+    this.params.pointSize = value;
+    this.updateStream();
+  }
+
+  get thresholdX() { return new Vector2(this.params.thresholdX.min, this.params.thresholdX.max); }
+
+  set thresholdX(value) {
+    if (!value) return;
+
+    this.params.thresholdX.min = value.x;
+    this.params.thresholdX.max = value.y;
+    this.updateStream();
+  }
+
+  get thresholdY() { return new Vector2(this.params.thresholdY.min, this.params.thresholdY.max); }
+
+  set thresholdY(value) {
+    if (!value) return;
+
+    this.params.thresholdY.min = value.x;
+    this.params.thresholdY.max = value.y;
+    this.updateStream();
+  }
+
+  get thresholdZ() { return new Vector2(this.params.thresholdZ.min, this.params.thresholdZ.max); }
+
+  set thresholdZ(value) {
+    if (!value) return;
+
+    this.params.thresholdZ.min = value.x;
+    this.params.thresholdZ.max = value.y;
+    this.updateStream();
+  }
+
   onAdd() {
-    console.log("vptstream onAdd " + this._src);
+    console.log("vptstream onAdd " + this.params.src);
   }
 
   onChange() {
-    console.log("vptstream onChange " + this._src);
+    //console.log("vptstream onChange " + this.params.src);
+    this.updateStream();
   }
 
   onSelect() { }
@@ -114,18 +183,18 @@ export default class VPTStreamNode extends EditorNodeMixin(Object3D) {
 
   async loadMedia() {
 
-    if (!this._src || this._src.length < 5) {
+    if (!this.params.src || this.params.src.length < 5) {
       console.error("vptstream invalid src")
       return;
     }
 
-    if (!this._meta || this._meta.length < 5) {
+    if (!this.params.meta) {
       console.error("vptstream invalid meta")
       return;
     }
 
-    let url = this._src;
-    const fileExtension = url.substr((this._src.lastIndexOf('.') + 1));
+    let url = this.params.src;
+    const fileExtension = url.substr((this.params.src.lastIndexOf('.') + 1));
 
     if (fileExtension != "m3u8") {
       try {
@@ -142,10 +211,23 @@ export default class VPTStreamNode extends EditorNodeMixin(Object3D) {
 
     const params = {
       videoPath: proxySrc,
-      meta: this._meta,
-      renderMode: 'perspective',
+      meta: this.params.meta,
+      renderMode: this.params.renderMode,
+      pointSize: this.params.pointSize,
+      scale: this.scale.z
     }
     this.vptstream.load(params);
+  }
+
+  updateStream() {
+
+    if (this.vptstream) {
+
+      this.vptstream.updateParameter("pointSize", this.params.pointSize);
+
+      this.vptstream.updateParameter("scale", this.scale.z);
+
+    }
 
   }
 
@@ -165,16 +247,19 @@ export default class VPTStreamNode extends EditorNodeMixin(Object3D) {
       }
     }
 
-    this._src = source.src;
-    this._meta = source.meta;
+    this.params.src = source.src;
+    this.params.meta = source.meta;
     return this;
   }
 
   serialize() {
     return super.serialize({
       "vpt-stream": {
-        src: this._src,
-        meta: this._meta
+        src: this.params.src,
+        meta: this.params.meta,
+        renderMode: this.params.renderMode,
+        pointSize: this.params.pointSize,
+        scale: this.params.scale
       }
     });
   }
@@ -182,8 +267,11 @@ export default class VPTStreamNode extends EditorNodeMixin(Object3D) {
   prepareForExport() {
     super.prepareForExport();
     this.addGLTFComponent("vpt-stream", {
-      src: this._src,
-      meta: this._meta
+      src: this.params.src,
+      meta: this.params.meta,
+      renderMode: this.params.renderMode,
+      pointSize: this.params.pointSize,
+      scale: this.params.scale
     });
     this.replaceObject();
   }
--- src/ui/properties/VPTStreamNodeEditor.js
+++ src/ui/properties/VPTStreamNodeEditor.js
@@ -3,9 +3,22 @@ import PropTypes from "prop-types";
 import NodeEditor from "./NodeEditor";
 import InputGroup from "../inputs/InputGroup";
 import StringInput from "../inputs/StringInput";
-import StringInputTall from "../inputs/StringInput";
+import CompoundNumericInput from "../inputs/CompoundNumericInput";
+import Vector2Input from "../inputs/Vector2Input";
+import SelectInput from "../inputs/SelectInput";
 
 import { Link } from "styled-icons/fa-solid/Link";
+
+const renderModeOptions = [
+  { label: "Perspective Points", value: "perspective" },
+  { label: "Cutout 2D", value: "cutout" }
+];
+
+const cameraOptions = [
+  { label: "Realsense D415", value: "realsenseD415" },
+  { label: "Azure Kinect", value: "azurekinect" },
+  { label: "Custom", value: "custom" }
+];
 
 export default class VPTStreamNodeEditor extends Component {
   static propTypes = {
@@ -21,8 +34,28 @@ export default class VPTStreamNodeEditor extends Component {
     this.props.editor.setPropertySelected("src", src);
   };
 
-  onChangeMeta = meta => {
-    this.props.editor.setPropertySelected("meta", meta);
+  onChangeCamera = sensor => {
+    this.props.editor.setPropertySelected("sensor", sensor);
+  };
+
+  onChangeRenderMode = renderMode => {
+    this.props.editor.setPropertySelected("renderMode", renderMode);
+  };
+
+  onChangePointSize = pointSize => {
+    this.props.editor.setPropertySelected("pointSize", pointSize);
+  };
+
+  onChangeThresholdX = thresholdX => {
+    this.props.editor.setPropertySelected("thresholdX", thresholdX);
+  };
+
+  onChangeThresholdY = thresholdY => {
+    this.props.editor.setPropertySelected("thresholdY", thresholdY);
+  };
+
+  onChangeThresholdZ = thresholdZ => {
+    this.props.editor.setPropertySelected("thresholdZ", thresholdZ);
   };
 
   render() {
@@ -33,8 +66,53 @@ export default class VPTStreamNodeEditor extends Component {
         <InputGroup name="Stream URL">
           <StringInput value={node.src} onChange={this.onChangeSrc} />
         </InputGroup>
-        <InputGroup name="Meta json">
-          <StringInputTall value={node.meta} onChange={this.onChangeMeta} />
+        <InputGroup name="Camera/Sensor" info="What camera is use to capture depth">
+          <SelectInput options={cameraOptions} value={node.sensor} onChange={this.onCameraChange} />
+        </InputGroup>
+        <InputGroup name="Render Mode" info="Limit what type of media this frame will capture">
+          <SelectInput options={renderModeOptions} value={node.renderMode} onChange={this.onChangeRenderMode} />
+        </InputGroup>
+        <InputGroup name="Point Size">
+          <CompoundNumericInput
+            min={0.1}
+            max={20}
+            step={0.1}
+            value={node.pointSize}
+            onChange={this.onChangeStartOpacity}
+          />
+        </InputGroup>
+        <InputGroup name="Threshold X (width)">
+          <Vector2Input
+            smallStep={0.01}
+            mediumStep={0.1}
+            largeStep={0.25}
+            min={0}
+            max={4}
+            value={node.thresholdX}
+            onChange={this.onChangeThresholdX}
+          />
+        </InputGroup>
+        <InputGroup name="Threshold Y (height)">
+          <Vector2Input
+            smallStep={0.01}
+            mediumStep={0.1}
+            largeStep={0.25}
+            min={0}
+            max={4}
+            value={node.thresholdY}
+            onChange={this.onChangeThresholdY}
+          />
+        </InputGroup>
+        <InputGroup name="Threshold Z (depth)">
+          <Vector2Input
+            smallStep={0.01}
+            mediumStep={0.1}
+            largeStep={0.25}
+            min={0}
+            max={4}
+            value={node.thresholdZ}
+            onChange={this.onChangeThresholdZ}
+          />
         </InputGroup>
       </NodeEditor>
     );
